0d9fb39c3049720ce654b93eca52967cdfea09f7
bhupalan subramaniyam <bhupalans@gmail.com>
2025-08-30 08:34:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,45 +1,12 @@
 
 "use client"
 
-import * as React from "react";
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+const Collapsible = CollapsiblePrimitive.Root
 
-const OriginalCollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+const CollapsibleTrigger = CollapsiblePrimitive.Trigger
 
-const CollapsibleContent = React.forwardRef<
-  React.ElementRef<typeof OriginalCollapsibleContent>,
-  React.ComponentPropsWithoutRef<typeof OriginalCollapsibleContent> & { contentWrapper?: React.ElementType }
->(({ contentWrapper: Wrapper = "div", ...props }, ref) => {
-  return (
-    <OriginalCollapsibleContent
-      ref={ref}
-      asChild
-      {...props}
-    >
-      <Wrapper>{props.children}</Wrapper>
-    </OriginalCollapsibleContent>
-  )
-});
-CollapsibleContent.displayName = 'CollapsibleContent';
-
-const Collapsible = React.forwardRef<
-  React.ElementRef<typeof CollapsiblePrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Root> & { contentWrapper?: React.ElementType }
->(({ contentWrapper, ...props }, ref) => {
-  const children = React.Children.toArray(props.children);
-  const trigger = children.find((child: any) => child.type === CollapsibleTrigger || child.props.asChild);
-  const content = children.find((child: any) => child.type === CollapsibleContent);
-
-  return (
-    <CollapsiblePrimitive.Root {...props} ref={ref}>
-      {trigger}
-      {content && React.cloneElement(content as React.ReactElement, { contentWrapper })}
-    </CollapsiblePrimitive.Root>
-  )
-});
-Collapsible.displayName = "Collapsible"
-
+const CollapsibleContent = CollapsiblePrimitive.Content
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
