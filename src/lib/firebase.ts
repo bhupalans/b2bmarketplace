5791b870a16362feb5edf10d4e9068ece7324125
bhupalans <72866845+bhupalans@users.noreply.github.com>
2026-01-11 12:03:18 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ import { processAcceptedOffer } from '@/app/user-actions';
 import { areDetailsEqual, areSpecificationsEqual } from './utils';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDL_o5j6RtqjCwFN5iTtvUj6nFfyDJaaxc",
+  apiKey: "Enter your API key",
   authDomain: "b2b-marketplace-udg1v.firebaseapp.com",
   projectId: "b2b-marketplace-udg1v",
   storageBucket: "b2b-marketplace-udg1v.firebasestorage.app",
